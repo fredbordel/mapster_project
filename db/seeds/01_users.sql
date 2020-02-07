98f97db293cb919cf3cede73b024ed5ad284f280
ChanTheo <theo.james.chan@gmail.com>
2020-02-07 22:57:07 +0000

    added some boilerplate users into a seed script for the db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (username, email, password, name)
+VALUES ('Dragon_Mess', 'Camilo@lighthouse.ca', 'password','Camillo' ),
+('User_Example', 'User123@example.ca', 'password', 'Example');
+-- INSERT INTO users (name) VALUES ('Kira');
