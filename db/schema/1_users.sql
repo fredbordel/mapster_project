61e8e4d1f0877e9fd4be16ebf6db6f9678164e30
ChanTheo <theo.james.chan@gmail.com>
2020-02-08 18:11:24 +0000

    tables for database created

--- db/schema/1_users.sql
+++ db/schema/1_users.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  username TEXT NOT NULL
+  username TEXT NOT NULL,
   email TEXT NOT NULL,
   password TEXT NOT NULL,
   name TEXT NOT NULL
---
+++ db/schema/5_addresses.sql
@@ -0,0 +1,10 @@
+-- DROP TABLE IF EXISTS addresses;
+
+-- CREATE TABLE addresses (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   street_number INTEGER,
+--   street TEXT,
+--   city TEXT,
+--   postcode TEXT
+--   point_id INTEGER REFERENCES
+-- );
