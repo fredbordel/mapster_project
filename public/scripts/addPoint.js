730d3e9fd0b7adba38f33e9ed871779786463124
ChanTheo <theo.james.chan@gmail.com>
2020-02-10 21:21:43 +0000

    added a post route for the create/map page

---
+++ public/scripts/addPoint.js
@@ -0,0 +1,32 @@
+
+// const L = require('leaflet');
+
+
+// L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/'
+
+// $(document).ready(
+
+//   $(".create__map").click(function(event) {
+
+//   })
+// // );
+// $(() => {
+// const map = L.map('map').setView([51.505, -0.09], 13);
+// L.marker([51.5, -0.09]).addTo(map);
+
+// function onMapClick (e) {
+//   let popup = L.popup();
+//   console.log("got in the mapclick function")
+//   popup
+//   .setLatLng(e.latlng)
+//   .setContent()
+//   .openOn(map);
+// }
+
+//  map.on('click', onMapClick)
+// })
+
+
+// module.exports = {
+//   mapClickPopup
+// };
