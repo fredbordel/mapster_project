766fcba255547569e8f72e8eba457776abe95562
vagrant <frederique.bordeleau@gmail.com>
2020-02-11 20:08:16 +0000

    Creating function that creates a map for each map in the database

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,7 +13,6 @@ $(() => {
 
 
   let mymap = L.map("mymap").setView([45.50, -73.56], 11);
-
   L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=7UYb6bOCvUG7YuJGjcqG', {
   attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
   maxZoom: 18,
@@ -21,7 +20,7 @@ $(() => {
   accessToken: 'your.mapbox.access.token'
   }).addTo(mymap);
 
-  L.marker([45.50, -73.56]).addTo(mymap);
+
 
   $(".create__map").submit(function (event){
     event.preventDefault();
@@ -54,6 +53,8 @@ $(() => {
 //________________________________|
 
 let createNewMap = function(mapData) {
+
+
 
   const newMap = `
   <section class="container__map">
