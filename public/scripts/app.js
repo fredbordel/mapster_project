457646cc6ce5da89f709faacc2dd25cb5ec3b1c7
vagrant <frederique.bordeleau@gmail.com>
2020-02-11 22:07:36 +0000

    Fxing problems. Now theo an I are goodgit add .

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -73,53 +73,6 @@ $(() => {
 
 
 
-  //_________________________________
-// FUNCTION THAT CREATES A NEW MAP
-//________________________________|
-
-let createNewMap = function(mapData) {
-
-
-
-  const newMap = `
-  <section class="container__map">
-  <header class="map__header">
-      <span class="map__header__title">${mapData.title}</span>
-      <span class="map__header__handle">@EMPTYFORNOW</span>
-    </header>
-  <div id="mymap"></div>
-  <script>
-    let mymap = L.map("mymap").setView([${mapData.latitude}, ${mapData.longitude}], ${mapData.zoom_level});
-    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=7UYb6bOCvUG7YuJGjcqG', {
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    accessToken: 'your.mapbox.access.token'
-    }).addTo(mymap);
-    </script>
-    <footer class="map__footer">
-        <button class="map__footer__modify">MODIFY THIS MAP</button>
-        <button class="map__footer__favorite">ADD TO FAVORITE</button>
-      </footer>
-  </section>
-  `
-  return newMap;
-  };
-
-  //_____________________________________
-    // FUNCTIONS LOAD NEW MAP TO INDEX FEED|
-  //_____________________________________|
-
-
-
-
-
-});
-
-
-
-
-
 //Function that adds points to an existing map
 // how to differentiate between the diffrent maps across pages
 // how to add multiple poiints? a submit and a reset button at the bottom of the form?
@@ -165,9 +118,7 @@ $('#create_point_button').click(function(){
   // pageload ends here
 });
 
-//_________________________________
-// FUNCTION THAT CREATES A NEW MAP
-//________________________________|
+
 
 
 
