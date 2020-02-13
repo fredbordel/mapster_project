3ac8ccc6f8e1d5ac5fe3ebf053d474ad2ecd55a7
vagrant <frederique.bordeleau@gmail.com>
2020-02-13 17:38:43 +0000

    Creating the favorite feature, but still struggling with map id from leaflet api rules

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,18 +4,34 @@ const router  = express.Router();
 module.exports = (db) => {
 
   router.get("/favorites", (req, res) => {
-    db.query(`SELECT * FROM favorites WHERE map_id = ${req.body.id}`)
-    .then(data => res.json({error:false, new_point: data}))
-    .catch(data => res.json({error:true, new_point: data}))
+    db.query(`SELECT * FROM maps WHERE id IN (SELECT map_id FROM favorites WHERE user_id = $1)`, [1])
+    .then((response) => {res.send(response.rows);})
+    .catch(data => res.json({error:true, favorite: data}))
   });
 
   router.post("/favorites", (req, res) => {
     values = [req.body.id, 1]
     db.query(`INSERT INTO favorites (map_id, user_id) VALUES ($1, $2) RETURNING *`, values)
-    .then(data => res.json({error:false, new_point: data}) )
-    .catch(data => res.json({error:true, new_point: data}) )
+    .then(data => res.json({error:false, favorite: data}) )
+    .catch(data => res.json({error:true, favorite: data}) )
     })
+
+// IM TRYING HERE
+
+
+// router.get("/favorites", (req, res) => {
+//    db.query(`SELECT * FROM maps WHERE id IN (SELECT map_id  FROM favorites ORDER BY id DESC LIMIT 1);`)
+//     .then((response) => res.send(response.rows))
+// });
+
+
+// IM TRYING HERE
+
+
+
   return router;
+
+
 };
 
 
